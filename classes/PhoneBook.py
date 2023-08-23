e5f89bd9b1866526408165292d94cb63e8585df9
Ilia Loginov <saft48@yandex.ru>
2023-08-23 12:52:17 +0300

    0.5 - editing_contacts

--- classes/PhoneBook.py
+++ classes/PhoneBook.py
@@ -29,7 +29,6 @@ class PhoneBook:
 
             if user_choice == 1:
                 self.get_contacts_one()
-                print("Контакт добавлен!")
 
             elif user_choice == 2:
                 user_page_choice = input("Желаете ли выбрать длину списка? По умолчанию - 5 (Y/N): ").lower()
@@ -72,7 +71,10 @@ class PhoneBook:
 
         try:
             with open(self.__db_file, mode="r", encoding="utf-8") as file:
-                last_contact_num = int(file.readlines()[-1].split("|")[0]) + 1
+                try:
+                    last_contact_num = int(file.readlines()[-1].split("|")[0]) + 1
+                except IndexError:
+                    last_contact_num = 1
         except (FileNotFoundError, FileExistsError):
             print("Что-то пошло не так. Обратитесь к разработчику за дополнительной информацией, всё скоро починят!")
             return
@@ -86,19 +88,26 @@ class PhoneBook:
 
     def get_contacts_one(self):
         """Интерфейс просмотра контактов по одному"""
+        continue_from_edited_contact = False
+        while True:
 
-        with open(self.__db_file, mode="r", encoding="utf-8") as file:
-            contacts_by_line = file.readlines()
+            with open(self.__db_file, mode="r", encoding="utf-8") as file:
+                contacts_by_line = file.readlines()
 
-            i_contact = 0
+            if not continue_from_edited_contact:
+                i_contact = 0
+            else:
+                continue_from_edited_contact = False
+
             while i_contact < len(contacts_by_line):
                 contact_list = contacts_by_line[i_contact].strip("\n").split("|")
 
-                contact = Contact(contact_list[0], contact_list[1], contact_list[2],
-                                  contact_list[3], contact_list[4], contact_list[5])
+                id_contact = contact_list[0]
+                contact = Contact(contact_list[1], contact_list[2], contact_list[3],
+                                  contact_list[4], contact_list[5], contact_list[6])
 
                 print(contact, '\n')
-                user_choice = int(input("Далее - 1;\nНазад - 2;\nЗакончить - 3;\nВвод: "))
+                user_choice = int(input("Далее - 1;\nНазад - 2;\nРедактирование - 3\nЗакончить - 4;\nВвод: "))
                 if user_choice == 1:
                     i_contact += 1
                     if i_contact == len(contacts_by_line):
@@ -110,8 +119,12 @@ class PhoneBook:
                         print("Это и есть самый первый контакт")
                         i_contact += 1
                 elif user_choice == 3:
+                    if self.edit_contact(id_contact=id_contact):
+                        continue_from_edited_contact = True
+                        break
+                elif user_choice == 4:
                     print("Возвращаемся в главное меню")
-                    break
+                    return
                 else:
                     print("Неизвестная команда")
 
@@ -169,12 +182,48 @@ class PhoneBook:
                 else:
                     print("Неизвестная команда")
 
-    def change_contact(self):
-        ...
+    def edit_contact(self, id_contact: str):
+        """"""
+        edit_contact_menu = (None, "Имя", "Фамилию", "Отчество", "Организацию", "Рабочий телефон", "Личный телефон")
+        rus_suffixes = (None, "ое", "ую", "ое", "ую", "ый", "ый")
+
+        while True:
+            print("Что желаете отредактировать?\n"
+                  f"1 - {edit_contact_menu[1]}\n"
+                  f"2 - {edit_contact_menu[2]}\n"
+                  f"3 - {edit_contact_menu[3]}\n"
+                  f"4 - {edit_contact_menu[4]}\n"
+                  f"5 - {edit_contact_menu[5]}\n"
+                  f"6 - {edit_contact_menu[6]}\n"
+                  f"7 - Отмена")
+            user_choice = int(input("Ваш ввод: "))
+            if user_choice in range(1, 7):
+                break
+            elif user_choice == 7:
+                return
+            else:
+                print("Неизвестная команда. Введите команду заново")
+
+        user_edit_input = input(f"Введите нов{rus_suffixes[user_choice]} {edit_contact_menu[user_choice]}: ")
+        with open(self.db_file, mode="r", encoding="utf-8") as file:
+            all_contacts = file.readlines()
+            for i_contact in all_contacts:
+                if i_contact.split("|")[0] == id_contact:  # Here we detect the needed line by id
+                    editing_contact_list = all_contacts[int(id_contact) - 1].split("|")
+                    editing_contact_list[user_choice] = user_edit_input
+                    all_contacts[int(id_contact) - 1] = "|".join(editing_contact_list)  # the whole list is split!
+        with open(self.db_file, mode="w+", encoding="utf-8") as file:
+            for j_contact in all_contacts:
+                file.write(j_contact)
+            return True
 
     def check_contact_line(self):
         """Заглушка - ф-ция, если я захочу вводить ID-номера контактов"""
         pass
+
+    @property
+    def db_file(self) -> str:
+        return self.__db_file
 
 
 if __name__ == '__main__':
